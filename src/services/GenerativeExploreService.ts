254c91ff52a73aa5fbad70423c6b896039345372
Ricardo Geh <gricardo@google.com>
2023-08-17 10:36:26 -0300

    Change Explore to use BQML.GENERATE_TEXT

--- src/services/GenerativeExploreService.ts
+++ src/services/GenerativeExploreService.ts
@@ -1,4 +1,4 @@
-import { ILookmlModelExploreField, ISqlQueryCreate, IWriteQuery, Looker40SDK, user} from "@looker/sdk";
+import { ILookmlModelExploreField, ISqlQueryCreate, IWriteQuery, Looker40SDK, run_url_encoded_query, user} from "@looker/sdk";
 import { UtilsHelper } from "../utils/Helper";
 import { LookerSQLService } from "./LookerSQLService";
 import { IDictionary } from "@looker/sdk-rtl";
@@ -50,25 +50,33 @@ export class GenerativeExploreService {
     {
         switch(promptType){
             case PromptType.FIELDS_FILTERS_PIVOTS_SORTS:
-                return `
-LookerLLM Context: ${serializedModelFields}
-Question: ${userInput} 
-Act as an experienced data analyst that can extract field names based on the business Question.
-Using the LookerLLM Context with the field names, extract the desired field names, filters, pivots and sorts from the question in a JSON format.
-Valid fields are fields, filters, pivots and sorts.
-Whenever a period restriction is mentioned, identify it as a filter.
-Filter format follow the examples below:
-Examples:
-Q: orders in the last month
-{"filters": {"order_items.created_month": "last month"}}
-Q: Orders have more than 45 items.
-{"filters": {"order_items.count": "> 15"}}
-Q: Orders Created in the past 7 days
-{"filters": {"sales_order.created_date": "7 days"}}
-Q: states: California, Nevada, Washinton and Oregon.
-{"filters": {"sales_order.state": "California, Nevada, Washington, Oregon"}}
-Q: e-mail adress is not null
-{"filters": {"user.email_address": "-NULL"}}
+                return `Context: ${serializedModelFields}
+Question: ${userInput}
+
+Extract the exact fields, filters, pivots, explicit_pivots from the Context in a JSON format that can help answer the Question.The fields are in the format "table.field".
+explicit_pivots are the fields that are mentioned explicitly after the word "pivot" or "pivoting" keyword inside the Question.            
+Whenever the question contains a count or total, include a count inside the fields.
+
+{
+    "fields": [],
+    "filters": {},
+    "pivots": [],
+    "explicit_pivots": [],
+    "sorts": []
+}
+
+Examples:            
+Q: "What are the top 10 total sales price per brand. With brands: Levi\\'s, Calvin Klein, Columbia"
+{"fields":["products.brand","order_items.total_sale_price"],"filters":{"products.brand":"Levi\\'s, Calvin Klein, Columbia"}}
+
+Q: "What are the top sales price, category, cost pivot per day and filter only orders with more than 15 items"
+{"fields":["order_items.total_sale_price", "products.category", "inventory_items.cost"], "pivots": ["orders.created_date"], "filters": {"order_items.count": "> 15"}}
+
+Q: "How many orders were created in the past 7 days"
+{"fields": ["orders.count"], "filters": {"sales_order.created_date": "7 days"}}
+
+Q: "What are the states that had the most orders, filter state: California, Nevada, Washinton, Oregon"
+{"fields": ["orders.count"], "filters": {"sales_order.state": "California, Nevada, Washington, Oregon"}}
 `;
             case PromptType.FILTERS:
                 return `
@@ -138,7 +146,7 @@ Q: What are the total sales per month?
         return shardedPrompts;
     }
 
-    private removeInexistentFields(
+    private validateLLMFields(
         modelFields: FieldMetadata[],
         llmFields: Array<string>
     ): Array<string>
@@ -159,32 +167,43 @@ Q: What are the total sales per month?
                 }
             }
         }
-        console.log("Input1 eram: " + llmFields.length + " Output: " + cleanLLMFields.length);
+        console.log("Input1 eram: " + JSON.stringify(llmFields) + " Output: " + JSON.stringify(cleanLLMFields));
         return cleanLLMFields;
     }
 
-    private removeInexistentFieldsFromDict(
+    private validateFilterFormatValue(filterValue: string):string
+    {
+        var cleanFilterValue = filterValue.replace("_", " ");
+        cleanFilterValue = cleanFilterValue.replace("-", " ");
+        // validate and replace other invalid patterns
+        return cleanFilterValue;
+    }
+
+    private validateLLMFilters(
         modelFields: FieldMetadata[],
-        llmFields: IDictionary<string>
+        llmFilters: IDictionary<string>
     ): IDictionary<string>
     {
-        const cleanLLMFields: IDictionary<string> = {};        
+        const cleanLLMFields: IDictionary<string> = {};
         for(const modelField of modelFields )
         {            
-            if(modelField.name!= null && llmFields!=null)
+            if(modelField.name!= null && llmFilters!=null)
             {
-                for(const key of Object.keys(llmFields))
+                for(const key of Object.keys(llmFilters))
                 {            
                     if(key == modelField.name)
                     {
-                        console.log("LLMField equals modelField.name")
-                        cleanLLMFields[key] = llmFields[key];
+                        // Validate Filter Values
+                        if(this.validateFilterFormatValue(llmFilters[key]) != "")
+                        {
+                            cleanLLMFields[key] = llmFilters[key];                        
+                        }                        
                         break;
                     }
                 }
             }
         }
-        console.log("Input Dict eram: " + llmFields.length + " Output: " + cleanLLMFields.length);
+        console.log("Input Dict eram: " + llmFilters.length + " Output: " + cleanLLMFields.length);
         return cleanLLMFields;
     }
 
@@ -199,11 +218,11 @@ Q: What are the total sales per month?
             ${selectPrompt}
             ),
             STRUCT(
-            0.2 AS temperature,
+            0.05 AS temperature,
             1024 AS max_output_tokens,
-            0.95 AS top_p,
+            0.98 AS top_p,
             TRUE AS flatten_json_output,
-            40 AS top_k));
+            1 AS top_k));
         `;
     }
 
@@ -233,73 +252,63 @@ Q: What are the total sales per month?
          return results;
     }
 
-
-    private async findFiltersFromLLM(
-        modelFields: FieldMetadata[],
-        userFilter: string): Promise<IDictionary<string>>    
-    {
-         // First generate prompt for Fields
-         const promptFilters:Array<string> = this.generatePrompt(modelFields, userFilter, PromptType.FILTERS);
-         const results = await this.retrieveLookerParametersFromLLM(promptFilters);
-         var filtersToUse:IDictionary<string> = {};
-         for(var result of results)
-         {
-             try {
-                 if(result!=null && result.r != null && result.r.length > 0)
-                 {
-                     var llmResultLine = JSON.parse(result.r);
-                     const filters = llmResultLine.filters;
-                     for (const key in filters)
-                     {
-                        filtersToUse[key] = filters[key];
-                     }
-                 }
-                 else{
-                    if(result.status!=null)
-                    {
-                        console.log("result not found, status: "+ result.status);
-                    }
-                     console.log("Not found any fields:");
-                 }       
-                 console.log(filtersToUse);                         
-             } catch (err) {
-                 console.log("Invalid JSON parse: " + result);
-                //  Ignoring this return and work with the others
-                //  throw new Error('LLM result does not contain a valid JSON');
-             }
-         }
-         //Remove filters that does not exists
-         filtersToUse = this.removeInexistentFieldsFromDict(modelFields, filtersToUse);
-         
-         return filtersToUse;
-    }
-
     private async getExplorePayloadFromLLM( 
         modelFields: FieldMetadata[],
-        userInput: string, 
-        userFilter?: string,
-        userPivot?:string): Promise<Array<string>>
+        userInput: string): Promise<{
+            fields: Array<string>,
+            filters: IDictionary<string>,
+            sorts: Array<string>,
+            pivots: Array<string>
+        }>
     {
         // Generate the Base Prompt
         const fieldsPrompts:Array<string> = this.generatePrompt(modelFields, userInput, PromptType.FIELDS_FILTERS_PIVOTS_SORTS);
         const results = await this.retrieveLookerParametersFromLLM(fieldsPrompts);
         var arrayLLMFields:Array<string> = [];
         var filtersToUse:IDictionary<string> = {};
+        var arrayPivots:Array<string> = [];
+        var arraySorts:Array<string> = [];
+
+        // Read from multiple shards
         for(var result of results)
         {
-            try {
+            try {                
                 if(result!=null && result.r != null && result.r.length > 0)
                 {
-                    var llmResultLine = JSON.parse(result.r);                                    
-                    arrayLLMFields = arrayLLMFields.concat(llmResultLine.fields);
-                    const filters = llmResultLine.filters;
-                    for (const key in filters)
+                    var llmResultLine = JSON.parse(result.r);
+                    if(llmResultLine.fields != null && llmResultLine.fields.length > 0)
                     {
-                       filtersToUse[key] = filters[key];
+                        arrayLLMFields = arrayLLMFields.concat(llmResultLine.fields);
                     }
+                    if(llmResultLine.filters !=null)
+                    {
+                        const filters = llmResultLine.filters;
+                        for (const key in filters)
+                        {
+                           filtersToUse[key] = filters[key];
+                        }
+                    }
+                    // Explicit pivot or pivotting with user input
+                    if(llmResultLine.pivots != null)
+                    {
+                        // bring the pivots also to the fields
+                        arrayLLMFields = arrayLLMFields.concat(llmResultLine.pivots);
+                        if(this.validateInputForPivots(userInput))
+                        {
+                            arrayPivots = arrayPivots.concat(llmResultLine.pivots);
+                        }                        
+                    }                
+                    if(llmResultLine.sorts != null)
+                    {
+                        arrayLLMFields.concat(llmResultLine.pivots);
+                    }                
+                    if(llmResultLine.sorts != null)
+                    {
+                        arraySorts = arraySorts.concat(llmResultLine.sorts);
+                    }                   
                 }
                 else{
-                    console.log("Not found any fields");
+                    console.log("Not found any JSON results from LLM");
                 }                                
             } catch (err) {
                 console.log(result);
@@ -307,15 +316,31 @@ Q: What are the total sales per month?
             }
         }
         //Remove fields that does not exists
-        arrayLLMFields = this.removeInexistentFields(modelFields, arrayLLMFields);
-        filtersToUse = this.removeInexistentFieldsFromDict(modelFields, filtersToUse);
+        arrayLLMFields = this.validateLLMFields(modelFields, arrayLLMFields);        
+        filtersToUse = this.validateLLMFilters(modelFields, filtersToUse);
+        arrayPivots = this.validateLLMFields(modelFields, arrayPivots);
+        arraySorts = this.validateLLMFields(modelFields, arraySorts);
 
         // Recheck with the LLM with the selected fields and modelFields if they are good to go or will eliminate some fields
         if(arrayLLMFields.length > 2)
         {
             // TODO: recheck with LLM if the fields makes sense;
         }
-        return arrayLLMFields;
+        return {
+            fields: arrayLLMFields,
+            filters: filtersToUse,
+            pivots: arrayPivots,
+            sorts: arraySorts
+        };        
+    }
+
+    private validateInputForPivots(userInput: string):boolean {
+        const inputUpper = userInput.toLocaleUpperCase();
+        if(inputUpper.includes("PIVOT") || inputUpper.includes("PIVOTTING")|| inputUpper.includes("PIVOTING"))
+        {
+            return true;
+        }
+        return false;
     }
 
    
@@ -357,28 +382,19 @@ Q: What are the total sales per month?
     }> {
 
         // Call LLM to find the fields
-        const fieldsFromLLM = await this.getExplorePayloadFromLLM(modelFields, userInput);
-        console.log("ArrayFields: " + fieldsFromLLM);
-
-        // Finding the filters
-        // var filtersFromLLM = {}
-        // if(userFilters!= null && userFilters.length > 0)
-        // {
-        //     filtersFromLLM = await this.findFiltersFromLLM(modelFields, userFilters);
-        // }
-        // console.log("fillters: " + JSON.stringify(filtersFromLLM))    
-
+        const payloadFromLLM = await this.getExplorePayloadFromLLM(modelFields, userInput);        
         // call LLM to ask for Limits
         const limitFromLLM = await this.findLimitsFromLLM(userInput);
-        console.log("limitFromLLM: " + limitFromLLM);        
 
         let llmQuery: IWriteQuery;
         try {
             llmQuery = {
                 model: inputModelName,
                 view: inputViewName,
-                fields: fieldsFromLLM,
-                // filters: filtersFromLLM,
+                fields: payloadFromLLM.fields,
+                filters: payloadFromLLM.filters,
+                pivots: payloadFromLLM.pivots,
+                sorts: payloadFromLLM.sorts,
                 limit: limitFromLLM
             };
             console.log("llmQuery: " + JSON.stringify(llmQuery));
