6459aaf883e806a23b014b2fb71009a48bb10592
Ricardo <gricardo@google.com>
2023-08-30 14:42:44 -0300

    Cleanup unused code

--- src/services/ConfigReader.ts
+++ src/services/ConfigReader.ts
@@ -1,11 +1,3 @@
-import { LookerSQLService } from "./LookerSQLService";
-import { Looker40SDK } from "@looker/sdk";
-
-export interface TopPrompts {
-    description: string;
-    prompt: string;
-    modelExplore: string;
-}
 export class ConfigReader {
     public static readonly CURRENT_VERSION = "1.2";
     public static readonly LAST_UPDATED = "08/24/2023";
