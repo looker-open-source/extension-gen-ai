45395e435dbc6604a1897aa1d7c8856183fcec0d
Kauê Gimenes <gimenes@google.com>
2023-08-31 10:14:59 -0300

    remove unused ConfigReader.DEFAULT_DATASET config

--- src/services/ConfigReader.ts
+++ src/services/ConfigReader.ts
@@ -1,6 +1,5 @@
 export class ConfigReader {
     public static readonly CURRENT_VERSION = "1.2";
     public static readonly LAST_UPDATED = "08/24/2023";
-    public static readonly DEFAULT_DATASET = "llm";
     public static readonly BQML_MODEL = "llm.llm_model";
 }   
