bb14d48b683a322878f6f3cfe5bb0061f33634dc
Kauê Gimenes <gimenes@google.com>
2023-08-31 09:51:53 -0300

    add jsdoc comment to UtilsHelper methods

--- src/utils/Helper.ts
+++ src/utils/Helper.ts
@@ -3,28 +3,42 @@ export interface IDictionary<T> {
 }
 
 export class UtilsHelper {
+    /**
+     * Adds an extra slash to line breaks \n -> \\n
+     * @param originalString
+     * @returns
+     */
     public static escapeBreakLine(originalString: string): string {
         return originalString
             .replace(/\n/g, '\\n');
     }
 
+    /**
+     * Adds an extra slash to line breaks \n -> \\n
+     * @param originalString
+     * @returns
+     */
     public static escapeSpecialCharacter(originalString: string): string {
-        // Fix Query with Escaping Single quote that is not escaped
         let fixedString = originalString;
-        // TODO: replace only when not already escaped
-        // const regex = /'(?!\\)/g;
-        // fixedString = originalString.replace(regex, "\\'");
         fixedString = fixedString.replace(/\'/g, "\\'");
         return fixedString;
     }
 
-
-
+    /**
+     * Returns first element from Array
+     * @param array
+     * @returns
+     */
     public static firstElement<T>(array: Array<T>): T {
         const [ firstElement ] = array;
         return firstElement;
     }
 
+    /**
+     * Replaces ```JSON with ```
+     * @param originalString
+     * @returns
+     */
     public static cleanResult(originalString: string): string {
         let fixedString = originalString;
         fixedString = fixedString.replace("```JSON", "");
@@ -32,8 +46,11 @@ export class UtilsHelper {
         return fixedString;
     }
 
-
-
+    /**
+     * Remove duplicates from Array
+     * @param array
+     * @returns
+     */
     public static removeDuplicates<T>(array: Array<T>): Array<T> {
         return Array.from(new Set(array));
     }
