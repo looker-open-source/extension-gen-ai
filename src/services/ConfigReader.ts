d1e022bba7a58f011be4c75a77f300bd7df47b96
Ricardo <gricardo@google.com>
2023-09-06 11:57:24 -0300

    Implemented feature to log the input, metadata and result of the LLM models

--- src/services/ConfigReader.ts
+++ src/services/ConfigReader.ts
@@ -2,4 +2,5 @@ export class ConfigReader {
     public static readonly CURRENT_VERSION = "1.3";
     public static readonly LAST_UPDATED = "09/05/2023";
     public static readonly BQML_MODEL = "llm.llm_model";
+    public static readonly EXPLORE_LOGGING = "llm.explore_logging";
 }   
