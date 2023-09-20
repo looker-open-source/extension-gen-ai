7f44406542142d93538abe5968916c526f547122
Ricardo <gricardo@google.com>
2023-09-20 19:14:12 -0300

    Fix Dashboard service to shard the tiles properly

--- src/services/ConfigReader.ts
+++ src/services/ConfigReader.ts
@@ -20,8 +20,8 @@
  */
 
 export class ConfigReader {
-    public static readonly CURRENT_VERSION = "1.3";
-    public static readonly LAST_UPDATED = "09/05/2023";
+    public static readonly CURRENT_VERSION = "1.4";
+    public static readonly LAST_UPDATED = "09/20/2023";
     public static readonly BQML_MODEL = "llm.llm_model";
     public static readonly EXPLORE_LOGGING = "llm.explore_logging";
 }   
