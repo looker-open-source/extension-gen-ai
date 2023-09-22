14f5de2b921a96cc54fb180a40a155718a175249
Ricardo <gricardo@google.com>
2023-09-22 08:37:38 -0300

    Fix LICENSE and license headers on all source code

--- src/views/Explore.tsx
+++ src/views/Explore.tsx
@@ -1,22 +1,9 @@
 /**
- * Copyright (c) 2023 Google LLC
+ * Copyright 2023 Google LLC
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
  */
 import React, { useContext, useEffect, useState , FormEvent, useCallback} from 'react'
 import { 
@@ -318,6 +305,9 @@ export const Explore: React.FC = () => {
           }                  
           <Span fontSize="medium">
             Any doubts or feedback or bugs, send it to <b>looker-genai-extension@google.com</b>
+          </Span>
+          <Span fontSize="small">
+          Public Documentation on: <a href="https://github.com/looker-open-source/extension-gen-ai" target="_blank">https://github.com/looker-open-source/extension-gen-ai</a>
           </Span>   
           <FieldSelect 
             id="topExamplesId"           
