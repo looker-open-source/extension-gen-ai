d61191ec9c71d26346a902f594a28263118b742c
Kauê Gimenes <gimenes@google.com>
2023-11-14 18:49:28 -0300

    fix webpack development config
    --disable-host-check was replaced by devServer.allowedHosts
    devServer.index was replaced by devServer.static

--- webpack.develop.js
+++ webpack.develop.js
@@ -25,13 +25,14 @@ module.exports = {
     ],
   },
   devServer: {
-    index: 'index.html',
+    static: ['dist'],
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
       'Access-Control-Allow-Headers':
         'X-Requested-With, content-type, Authorization',
     },
+    allowedHosts: "all"
   },
   plugins: [...commonConfig.plugins],
 }
