a0fc324da0dea594c750a2dbfda30d09e75bbcd8
Kauê Gimenes <gimenes@google.com>
2023-12-13 14:10:32 -0300

    fix deploystack install not executing preinit.sh
    after running some tests, the issue seems to be that the previous
    deploystack.yaml had some formatting issues, after recreating this file
    .deploystack/scripts are working again.

--- deployment/terraform/.deploystack/scripts/preinit.sh
+++ deployment/terraform/.deploystack/scripts/preinit.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 export PROJECT_ID=$(gcloud config get-value project)
-echo "ProjectId: $PROJECT_ID"
+echo "selecting active project ($PROJECT_ID)..."
+echo "making sure cloud resource manager service is enabled..."
 gcloud services enable cloudresourcemanager.googleapis.com
 export BUCKET_PREFIX="looker-gen-ai-tf-state-"
 export BUCKET_NAME="$BUCKET_PREFIX$PROJECT_ID"
 # Check if the bucket exists
 gsutil ls -b gs://$BUCKET_NAME
-
+echo "checking if tf state bucket exists ($BUCKET_NAME)"
 # If the bucket does not exist, create it
 if [ $? -ne 0 ]; then
  gsutil mb gs://$BUCKET_NAME
- echo "Bucket $BUCKET_NAME created."
+ echo "bucket $BUCKET_NAME was created..."
 else
- echo "Bucket $BUCKET_NAME already exists."
+ echo "bucket $BUCKET_NAME already exists..."
 fi
-
-sed -i "s/{PROJECTID}/$PROJECT_ID/g" ./deployment/main.tf
+sed -i "s/{STATE_BUCKET_NAME}/$BUCKET_NAME/g" ./deployment/main.tf
 echo "Replacing project id on main.tf"
 
